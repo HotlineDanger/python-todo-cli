c14332cce5c04d4a2aaf76046b15b1ac46e14773
HotlineDanger <teeb.c.ace@gmail.com>
2023-02-22 22:32:11 +0000

    compressor - wrote littel compressing function

--- zip_creator.py
+++ zip_creator.py
@@ -1,3 +1,12 @@
 import zipfile
 
-def make_archive():
+
+def make_archive(filepaths, dest_dir):
+    with zipfile.ZipFile(dest_dir + "/" + "compressed_zip", "w") as archive:
+        for filepath in filepaths:
+            archive.write(filepath)
+
+
+if __name__ == "__main__":
+    make_archive(filepaths=["gui.py", "cli.py"], dest_dir="dest")
+
