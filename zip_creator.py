388cb93eb4986391e7bd21ad78d75b6dbe404eb6
HotlineDanger <teeb.c.ace@gmail.com>
2023-02-23 22:30:29 +0000

    compressor - added pathlib

--- zip_creator.py
+++ zip_creator.py
@@ -1,8 +1,9 @@
 import zipfile
-
+import pathlib
 
 def make_archive(filepaths, dest_dir):
-    with zipfile.ZipFile(dest_dir + "/" + "compressed_zip", "w") as archive:
+    dest_path = pathlib.Path(dest_dir, "compressed.zip")
+    with zipfile.ZipFile(dest_path + "/" + "compressed_zip", "w") as archive:
         for filepath in filepaths:
             archive.write(filepath)
 
