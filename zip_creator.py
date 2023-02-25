20afede90ad33c58058b4d9cea57278cf6e5fad5
HotlineDanger <teeb.c.ace@gmail.com>
2023-02-25 22:14:28 +0000

    format path + zip dest

--- zip_creator.py
+++ zip_creator.py
@@ -3,7 +3,7 @@ import pathlib
 
 def make_archive(filepaths, dest_dir):
     dest_path = pathlib.Path(dest_dir, "compressed.zip")
-    with zipfile.ZipFile(dest_path + "/" + "compressed_zip", "w") as archive:
+    with zipfile.ZipFile(dest_path, "w") as archive:
         for filepath in filepaths:
             archive.write(filepath)
 
