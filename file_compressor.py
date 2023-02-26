e88065814aac5d21eae58ce3c2f7bca70edfc37b
HotlineDanger <teeb.c.ace@gmail.com>
2023-02-26 21:45:38 +0000

    compressor - correction on dest path + output message

--- file_compressor.py
+++ file_compressor.py
@@ -10,13 +10,15 @@ input2 = sg.Input()
 choose_button2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_button = sg.Button("Compress")
+output_label = sg.Text(key="output")
 window = sg.Window("File compressor", layout=[[label1, input1, choose_button1],
                                               [label2, input2, choose_button2],
-                                              [compress_button]])
+                                              [compress_button, output_label]])
 while True:
     event, values = window.read()
     print(event, values)
     filepaths = values["files"].split(";")
     folder = values["folder"]
     make_archive(filepaths, folder)
+    window["output"].update(value="Compression completed")
 window.close()
