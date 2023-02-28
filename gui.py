11d3abc2c56b43d46982ce8b4fa47d3a940dd328
HotlineDanger <teeb.c.ace@gmail.com>
2023-02-28 21:59:52 +0000

    gui todo wip

--- gui.py
+++ gui.py
@@ -2,10 +2,19 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter to-do")
+input_box = sg.InputText(tooltip="Enter to-do", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My To-Do app", layout=[[label], [input_box, add_button]], font=("Helvetica", 20))
-window.read()
-print("Bye")
+window = sg.Window("My To-Do app",
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    print("Bye")
+    match event:
+        case "Add":
+
 window.close()
